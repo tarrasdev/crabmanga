b2838b0601b7c1a5bb6bdd894ef15f642e2cf5c1
Taras <skoentoyar@gmail.com>
2018-02-08 22:39:48 +0200

    Update to Reader mode: navbar, link to title, minor style update

--- resources/views/gallery/show.blade.php
+++ resources/views/gallery/show.blade.php
@@ -1,15 +1,14 @@
-@include('reader/nav')
+@extends('reader/layout')
 
-    @if(count($post->galleries)>1)
-                @foreach($post->galleries as $image)
-                        <img src="{{$image->image}}" class='pages reader-mode'>
-                
-                @endforeach
-                        <button onclick='addImage(-1)'>Prev</button>
-                        <button onclick='addImage(1)'>Next</button>
-            @else
-                <p>Nothing to show here</p>
-    @endif
+@section('content')
 
-@include('reader/footer')
+@if(count($post->galleries)>1)
+            @foreach($post->galleries as $image)
+                    <img src="{{$image->image}}" class='pages reader-mode'>
+            @endforeach
+        @else
+            <p>Nothing to show here</p>
+@endif
+
+@endsection
 
--- resources/views/reader/nav.blade.php
+++ resources/views/reader/nav.blade.php
@@ -1,10 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Reader</title>
-    <link href="/css/crab.css" rel="stylesheet">
-</head>
-<body>
+<header>
+  <div class="navbar navbar-dark bg-dark box-shadow">
+    <div class="container d-flex justify-content-between">
+     
+        <div class='row'>
+            <div class='col'>
+                <a class='navbar-brand' href='/posts/{{$post->id}}'>{{$post->title}}</a>
+            </div>
+         
+            <div class='col'>
+                    <button onclick='addImage(-1)' class='reader-buttons'>Prev</button>
+                    <button onclick='addImage(1)' class='reader-buttons'>Next</button>
+            </div>
+          
+        </div>
+       
+    </div>
+  </div>
+</header>
